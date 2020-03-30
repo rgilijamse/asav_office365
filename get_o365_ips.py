dc0982a8e0abb9a283661b87e790bd1c59134f4e
Robin Gilijamse <robin@onezeroit.com>
2020-03-30 11:57:08 +0200

    fix pep8 errors

--- get_o365_ips.py
+++ get_o365_ips.py
@@ -1,27 +1,32 @@
 """
 Script to fetch Office365 IPs and present them for use in ASA ACLs
 """
-
 import uuid
-import requests
 import ipaddress
+import requests
 
 
 def generate_guid():
     """Randomly generate new GUID at runtime."""
-    id = str(uuid.uuid4())
-    return id
+    uid = str(uuid.uuid4())
+    return uid
+
+
+def assemble_url(services, IPv6):
+    """Assemble Office365 enpoint query URL for selected services."""
+    base_url = "https://endpoints.office.com/endpoints/worldwide"
+    guid = generate_guid()
+    service_list = ",".join(services)
+    no_ipv6 = str(not IPv6)
+
+    # assemble url
+    full_url = f"{base_url}?clientrequestid={guid}&ServiceAreas={service_list}&NoIPV6={no_ipv6}"
+    return full_url
 
 
 def get_o365_ips(services, IPv6):
     """Fetch Office365 IP-addresses for selected services."""
-    base_url = "https://endpoints.office.com/endpoints/worldwide"
-    guid = generate_guid()
-    service_list = ",".join(services)
-    NoIPv6 = str(not IPv6)
-
-    # assemble url
-    request_url = f"{base_url}?clientrequestid={guid}&services={service_list}&NoIPV6={NoIPv6}"
+    request_url = assemble_url(services, IPv6)
 
     # fetch IPs
     response = requests.get(request_url)
@@ -50,11 +55,11 @@ def prefix_to_network(prefix):
     return address, mask
 
 
-def ipList_to_objectGroup(ip_list):
+def ip_list_to_object_group(ip_list):
     """Translate IP prefix list to ASA network object-group."""
     object_group = list()
     object_group.append("object-group network o365_addresses")
-    
+
     for ip in ip_list:
         network, mask = prefix_to_network(ip)
         object_entry = f"  network-object {network} {mask}"
@@ -63,14 +68,13 @@ def ipList_to_objectGroup(ip_list):
     return object_group
 
 if __name__ == '__main__':
-    services = [ "Skype" ]  # allowed values: <Common | Exchange | SharePoint | Skype>
-    IPv6 = False  # allowed values: <True | False> 
+    SERVICES = ["Skype"]  # allowed values: <Common | Exchange | SharePoint | Skype>
+    IPV6 = False  # allowed values: <True | False>
 
-    response = get_o365_ips(services, IPv6)
+    o365_ips = get_o365_ips(SERVICES, IPV6)
+    prefix_list = parse_response(o365_ips)
 
-    ips = parse_response(response)
-
-    print("\n".join(ipList_to_objectGroup(ips)))
+    print("\n".join(ip_list_to_object_group(prefix_list)))
 
     #test_ip = "192.0.2.0/24"
     #print(prefix_to_network(test_ip))
